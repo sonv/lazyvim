ee0be1a01bd052d48142fb85852f6fefccd53c09
SV <truongson.vanp@gmail.com>
2026-02-13 23:25:01 +0700

    update

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,6 +5,7 @@ return {
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
     vim.g.vimtex_view_method = "sioyek"
+    vim.opt.conceallevel = 0
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
