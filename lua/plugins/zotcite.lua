e8bfda0589602819b8957c284afc878683241ddd
TSV <van.truongson@gmail.com>
2026-06-27 23:37:24 +0700

    update

--- lua/plugins/zotcite.lua
+++ lua/plugins/zotcite.lua
@@ -7,6 +7,8 @@ return {
   config = function()
     require("zotcite").setup({
       -- your options here (see doc/zotcite.txt)
+      zotero_sqlite_path = "/home/tsvan/Zotero/zotero.sqlite",
+      attach_dir = "/home/tsvan/Documents/References",
     })
   end,
 }
