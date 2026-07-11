1ffc41cb6122e9d5e1bfc079f51808053fb3c75b
Truong-Son Van <van.truongson@gmail.com>
2026-07-11 21:10:08 +0700

    update

--- lua/plugins/zotcite.lua
+++ lua/plugins/zotcite.lua
@@ -7,8 +7,8 @@ return {
   config = function()
     require("zotcite").setup({
       -- your options here (see doc/zotcite.txt)
-      zotero_sqlite_path = "~/Zotero/zotero.sqlite",
-      attach_dir = "~/Documents/References",
+      zotero_sqlite_path = vim.fn.expand("~/Zotero/zotero.sqlite"),
+      attach_dir = vim.fn.expand("~/Documents/References"),
     })
   end,
 }
